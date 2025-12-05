467c0a80d98efefe036019fecb9c9c6a565d90dd
andrizan <putra7a@gmail.com>
2025-12-05 17:22:02 +0700

    add alacritty

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 function starship_transient_prompt_func
   starship module character
 end
+# if use alacritty
+# function alac
+#   start alacritty.exe
+# end
 starship init fish | source
 enable_transience
